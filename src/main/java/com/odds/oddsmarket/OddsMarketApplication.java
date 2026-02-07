3d001555e5c40bca651c6f50c5e7691d5b5755c5
Krish Newatia <krish.n.newatia@oracle.com>
2026-02-07 13:25:39 +0530

    initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/odds/oddsmarket/OddsMarketApplication.java
@@ -0,0 +1,13 @@
+package com.odds.oddsmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddsMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OddsMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odds/oddsmarket/OddsMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odds.oddsmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddsMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
